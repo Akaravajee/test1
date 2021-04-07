f221fbcc61166a897e6ac82ee901693f15f43aca
Akaravajee <79188325+Akaravajee@users.noreply.github.com>
2021-04-07 16:00:04 +0700

    Update hellotest.py

--- hellotest.py
+++ hellotest.py
@@ -1,11 +1,11 @@
 from flask import Flask
-server = Flask(_name_)
+server = Flask(__name__)
 
 @server.route("/")
 def Home():
     return "<h1>อัคราวจี แจ้งศรี 6006021630032</h1>";
 
 
-if _name_ == '_main_':
+if __name__ == "__main__":
     #app.run(debug=True)
     server.run(host='0.0.0.0', port=80)
